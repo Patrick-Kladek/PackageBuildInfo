ed391ebb84192e821ea2b4f8c60943b727529dc1
Patrick-Kladek <patrick.kladek@outlook.com>
2025-08-13 22:16:54 +0200

    improve plugin

--- Plugins/PackageBuildInfoPlugin/plugin.swift
+++ Plugins/PackageBuildInfoPlugin/plugin.swift
@@ -12,31 +12,62 @@ struct PackageBuildInfoPlugin: BuildToolPlugin {
         guard let target = target as? SourceModuleTarget else { return [] }
         let outputFile = context.pluginWorkDirectory.appending("PackageBuild.swift")
 
-        let command: Command = .prebuildCommand(
-            displayName:
-                "Generating \(outputFile.lastComponent) for \(target.directory)",
-            executable:
-                try context.tool(named: "PackageBuildInfo").path,
-            arguments: [ "\(target.directory)", "\(outputFile)", target.moduleName ],
-            outputFilesDirectory: context.pluginWorkDirectory
+        let command: Command = .buildCommand(
+            displayName: "Generating \(outputFile.lastComponent) for \(target.directory)",
+            executable: try context.tool(named: "PackageBuildInfo").path,
+            arguments: [
+                "\(target.directory)", "\(outputFile)", target.moduleName
+            ],
+            inputFiles: [
+                context.package.directory.appending(".git")
+            ],
+            outputFiles: [
+                outputFile
+            ]
         )
+
+
+//        prebuildCommand(
+//            displayName:
+//                "Generating \(outputFile.lastComponent) for \(target.directory)",
+//            executable:
+//                try context.tool(named: "PackageBuildInfo").path,
+//            arguments: [ "\(target.directory)", "\(outputFile)", target.moduleName ],
+//            outputFilesDirectory: context.pluginWorkDirectory
+//        )
         return [command]
     }
 }
 
 #if canImport(XcodeProjectPlugin)
+
 import XcodeProjectPlugin
+
 extension PackageBuildInfoPlugin: XcodeBuildToolPlugin {
+
     func createBuildCommands(context: XcodeProjectPlugin.XcodePluginContext, target: XcodeProjectPlugin.XcodeTarget) throws -> [PackagePlugin.Command] {
         let outputFile = context.pluginWorkDirectory.appending("PackageBuild.swift")
+
         let command: Command = .prebuildCommand(
-            displayName:
-                "Generating \(outputFile.lastComponent) for \(context.xcodeProject.directory)",
-            executable:
-                try context.tool(named: "PackageBuildInfo").path,
+            displayName: "Generating \(outputFile.lastComponent) for \(context.xcodeProject.directory)",
+            executable: try context.tool(named: "PackageBuildInfo").path,
             arguments: [ "\(context.xcodeProject.directory)", "\(outputFile)", target.displayName ],
             outputFilesDirectory: context.pluginWorkDirectory
         )
+
+//        .buildCommand(
+//            displayName: "Generating \(outputFile.lastComponent) for \(context.xcodeProject.directory)",
+//            executable: try context.tool(named: "PackageBuildInfo").path,
+//            arguments: [
+//                "\(context.xcodeProject.directory)", "\(outputFile)", target.displayName
+//            ],
+//            inputFiles: [
+//                context.xcodeProject.directory
+//            ],
+//            outputFiles: [
+//                outputFile
+//            ])
+
         return [command]
     }
 }
